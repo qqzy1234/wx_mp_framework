61c36a781ae08795300ca4a565a971cd52c65828
Qiao <1584634690@qq.com>
2023-05-02 10:54:14 +0800

    添加示例代码

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -9,4 +9,29 @@ Page({
     onLoad() {
         
     },
+    watch: {
+        motto() {
+            console.log(arguments);
+            
+        }
+    },
+
+    eventListen: {
+        // 监听全部事件
+        onEvent() {},
+        // 监听点击事件
+        onTap(e: anyObj) {
+            console.log('点击了');
+        }
+    },
+
+    navigateTo(e: anyObj, self: anyObj, methods: anyObj) {
+        methods.router.navigateTo('/pages/logs/logs')
+    },
+    redirectTo(e: anyObj, self: anyObj, methods: anyObj) {
+        methods.router.redirectTo('/pages/logs/logs')
+    },
+    reLaunch(e: anyObj, self: anyObj, methods: anyObj) {
+        methods.router.reLaunch('/pages/logs/logs')
+    }
 })
